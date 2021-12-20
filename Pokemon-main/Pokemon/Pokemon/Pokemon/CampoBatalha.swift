fa073a227e9fb3cf4a04b3e38c185d78e850553d
SoniaFilipa <95963758+SoniaFilipa@users.noreply.github.com>
2021-12-20 17:14:16 +0000

    Pokemon v3

--- Pokemon-main/Pokemon/Pokemon/Pokemon/CampoBatalha.swift
+++ Pokemon-main/Pokemon/Pokemon/Pokemon/CampoBatalha.swift
@@ -1,20 +1,20 @@
 //
-//  Pokemon.swift
+//  CampoBatalha.swift
 //  Pokemon
 //
 //  Created by Developer on 11/12/2021.
 //
-import Foundation
+
 import SwiftUI
 
-struct Pokemon: View {
+struct CampoBatalha: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct Pokemon_Previews: PreviewProvider {
+struct CampoBatalha_Previews: PreviewProvider {
     static var previews: some View {
-        Pokemon()
+        CampoBatalha()
     }
 }
--- Pokemon-main/Pokemon/Pokemon/Pokemon/ContentView.swift
+++ Pokemon-main/Pokemon/Pokemon/Pokemon/ContentView.swift
@@ -8,64 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    /*
-    @State var Pokedex:[Pokemon] = [
-    Pokemon(nome: "Ditto",
-            nickName: "Joao",
-            img: "ditto"),
-    
-    Pokemon(nome: "Mew",
-            nickName: "Joao",
-            img: "mew"),
-    
-    Pokemon(nome: "Porygon",
-            nickName: "Joao",
-            img: "porygon"),
-    
-    Pokemon(nome: "Pikachu",
-            nickName: "Joao",
-            img: "pikachu")
-    ]*/
-    @State var Pokedex:[tipoPokemon] = [
-    
-    tipoPokemon(tipo: "Eletrico", pokemons: [Pokemon(nome: "Pikachu",nickName: "", img: "pikachu")]),
-    
-    
-    tipoPokemon(tipo: "Normal", pokemons: [Pokemon(nome: "Porygon",nickName: "", img: "porygon"),
-Pokemon(nome: "Ditto",nickName: "", img: "ditto") ]),
-    
-    tipoPokemon(tipo: "Psiquico", pokemons: [Pokemon(nome: "Mew",nickName: "", img: "mew")]),
-
-    ]
-    
     var body: some View {
-        NavigationView{
-            List{
-           
-                ForEach(Pokedex){ tipo in
-
-                    Section {
-                        ForEach(tipo.pokemons){ pokemon in
-                            NavigationLink {
-                                pokeDetalhes(pokemon: pokemon)
-                            } label: {
-                                ListLine(pokemon: pokemon)
-                            }
-                        }
-                        
-                    } header: {
-                        Text(tipo.tipo)
-                        
-                        
-                    }
-       
-            }
-            .navigationTitle("Pokedex")
-        }
-        
-        
+        Text("Hello, world!")
+            .padding()
     }
-}
 }
 
 struct ContentView_Previews: PreviewProvider {
